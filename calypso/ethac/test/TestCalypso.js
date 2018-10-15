9379b00039ce856afdb7f6021923d7365980f944
BjornGudmundsson <bjorn.gudmundsson95@gmail.com>
2018-10-15 00:26:28 +0000

    Finally all the test works. Changed the Calypso contract and all of the holder contracts to use addresses instead of hashes. Will probably remove all of the getID functions and use addresses instead. Added readRequests and a readRequestHolder and tested that a ReadRequest has to have a corresponding WriteRequest by comparing the address variable of the ReadRequest to all know write addresses.

--- calypso/ethac/test/TestCalypso.js
+++ calypso/ethac/test/TestCalypso.js
@@ -2,6 +2,7 @@ const  assert = require('assert');
 const ganache = require('ganache-cli');
 const Web3 = require("web3");
 const web3 = new Web3(ganache.provider());
+const cal = artifacts.require("./Calypso.sol");
 
 
 let accounts;
@@ -9,10 +10,22 @@ let accounts;
 let Calypso;
 
 const json = require("./../build/contracts/Calypso.json");
+const wrJSON = require("./../build/contracts/WriteRequest.json");
+const holderJSON = require("./../build/contracts/WriteRequestHolder.json");
+const readJSON = require("./../build/contracts/ReadRequest.json");
+const rrHolderJSON = require("./../build/contracts/ReadRequestHolder.json");
 
 const interface = json['abi'];
+const wrInterface = wrJSON['abi'];
+const holderInterface = holderJSON['abi'];
+const readInterface = readJSON['abi'];
+const rrHolderInterface = rrHolderJSON['abi'];
 
 const bytecode = json['bytecode'];
+const wrBytecode = wrJSON['bytecode'];
+const holderBytecode = holderJSON['bytecode'];
+const readBytecode = readJSON['bytecode'];
+const rrHolderBytecode = rrHolderJSON['bytecode'];
 
 beforeEach(async () => {
     accounts = await web3.eth.getAccounts();
@@ -22,13 +35,17 @@ beforeEach(async () => {
     const acc4 = accounts[4];
 
     const owners = [acc1, acc2, acc3, acc4];
+    const holder = await createHolder();
+    console.log("Holder address is: ", holder.options.address);
+    const rrHolder = await createReadHolder();
+    console.log("rrholder address is: ", rrHolder.options.address)
 
     Calypso = await new web3.eth.Contract(interface).deploy(
         {
             data : bytecode,
-            arguments : [owners],
+            arguments : [owners, holder.options.address, rrHolder.options.address],
         }
-    ).send({ from : accounts[0], gas : 3000000});
+    ).send({ from : accounts[0], gas : 4712388});
 
 });
 
@@ -38,29 +55,78 @@ it("Making sure that the owners contract and the Calypso contract integrate prop
 });
 
 it("Adding a writeRequest and then check that the transaction went through", async () => {
-    const d = "0x726984";
-    const ed = "0x548792";
-    const l = "0x578952";
     const acc1 = accounts[1];
     const acc2 = accounts[2];
     const acc3 = accounts[3];
     const acc4 = accounts[4];
-
-    const p = [acc1, acc2, acc3, acc4];
-    await Calypso.methods.addWriteRequest(d, ed, l, p);
-    console.log('Writerequest added');
-    const h = await Calypso.methods.getIDOfWriteRequest(d, ed, l, p).call(
-        {
-            gas : 4000000,
-        }
-    );
-    console.log("ID of writeRequest", h);
-    const isValid = await Calypso.methods.checkIfRequestIsValid(d, ed, l, p).call(
-        {
-            gas : 6000000,
-        }
-    );
-    console.log("is valid");
+    const d = "0x726984";
+    const ed = "0x548792";
+    const l = "0x578952";
+    const owners = [acc1, acc2, acc3, acc4];
+    const wr1 = await CreateWR(d, ed, l, owners);
+    const address = wr1.options.address;
+    
+    Calypso.methods.addWriteRequest(address).send({from : accounts[1], gas: 4712388})
+    const h = await Calypso.methods.getIDOfWriteRequest(address).call();
+    const isValid = await Calypso.methods.checkIfRequestIsValid(address).call();
     assert.equal(true, isValid);
 });
 
+it("Adding a ReadRequest with a corresponding WriteRequest", async () => {
+    const acc1 = accounts[1];
+    const acc2 = accounts[2];
+    const acc3 = accounts[3];
+    const acc4 = accounts[4];
+    const d = "0x726984";
+    const ed = "0x548792";
+    const l = "0x578952";
+    const owners = [acc1, acc2, acc3, acc4];
+    const wr = await CreateWR(d, ed, l, owners);
+    const wrAddress = wr.options.address;
+    const rr = await createReadRequest(wrAddress, accounts[0]);
+    const rrAddress = rr.options.address;
+    await Calypso.methods.addWriteRequest(wrAddress).send({from : accounts[1], gas: 4712388});
+    await Calypso.methods.AddReadRequest(rrAddress).send({from : accounts[1], gas: 4712388});
+    const canRead = await Calypso.methods.isValidReadRequest(rrAddress).call();
+    assert.equal(canRead, true);
+})
+
+
+const CreateWR = async (d, ed, l, a) => {
+    const data = [d, ed, l, a];
+    const wr1 = await new web3.eth.Contract(wrInterface).deploy(
+        {
+            data : wrBytecode,
+            arguments : data,
+        }
+    ).send({from : accounts[0], gas : 4712388})
+    return wr1;
+}
+
+const createHolder = async () => {
+    const h1 = await new web3.eth.Contract(holderInterface).deploy(
+        {
+            data : holderBytecode,
+        }
+    ).send({from : accounts[0], gas : 4712388});
+    return h1;
+}
+
+const createReadRequest = async (a1, a2) => {
+    const rr = await new web3.eth.Contract(readInterface).deploy(
+        {
+            data : readBytecode,
+            arguments : [a1, a2],
+        }
+    ).send({from : accounts[0], gas : 4712388});
+    return rr;
+}
+
+const createReadHolder = async () => {
+    const rrHolder = await new web3.eth.Contract(rrHolderInterface).deploy(
+        {
+            data : rrHolderBytecode,
+        }
+    ).send({from : accounts[0], gas : 4712388});
+    return rrHolder;
+}
